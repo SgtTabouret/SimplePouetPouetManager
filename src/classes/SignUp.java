971eb0f98be960b3228f283356745820f51c45d6
Mathieu DEGAND <mat.degand@numericable.fr>
2019-03-01 10:16:27 +0100

    debut inscription

---
+++ src/classes/SignUp.java
@@ -0,0 +1,53 @@
+package classes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SignUp
+ */
+@WebServlet("/signup")
+public class SignUp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignUp() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String login = req.getParameter("login");
+		String pwd = req.getParameter("password");
+		PrintWriter out = res.getWriter();
+		
+		if(login == null || login.equals("") || login.equals(" ")) {
+		out.println("<script>alert('le login ne peut pas être vide');</script>");
+		} 
+		if(pwd == null || pwd.equals("") || pwd.equals(" ")) {
+		out.println("<script>alert('le mot de passe ne peut pas être vide');</script>");
+		}
+		
+		
+	}
+
+}
